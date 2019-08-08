0aedf6105a9edca3a81cc31f41d2467c51d4fb6e
Ashley Norton <nortonashleym@gmail.com>
2019-08-08 01:38:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,3 +5,27 @@ end
 def using_unshift (arr, string)
   arr.unshift(string)
 end
+
+def using_pop (arr)
+  arr.pop
+end
+
+def pops_with_args (arr)
+  arr.pop(2)
+end
+
+def using_shift (arr)
+  arr.shift 
+end
+
+def shift_with_args (arr)
+  arr.shift(2)
+end
+
+def using_concat (arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert (arr, elem)
+  arr.insert(elem)
+  
